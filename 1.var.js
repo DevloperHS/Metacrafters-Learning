c179e28eee73d3b36a9bbb480aaabcfe8b9f5ae0
Purnendu Shukla(Harsh) <74095712+DevloperHS@users.noreply.github.com>
2023-03-06 18:48:10 +0530

    Add files via upload

---
+++ 1.var.js
@@ -0,0 +1,47 @@
+// declaring a variable
+var hello = "Hello";
+console.log("Hello")
+
+// assigning variable a value
+var num;
+num = 14;
+console.log(num) 
+
+// assigning variable a value to a new variable
+var num1;
+num1 = 14;
+var num2;
+num2 = num1;
+console.log("Num2", num2) 
+console.log("Num1", num1)
+
+// asingment of a variable in single line
+var num3 = 2015;
+console.log("Num3:",num3)
+
+//string variable intiatialization
+var string1 = "My Name Is Harsh"
+console.log(string1)
+
+//initalize a undefined variable
+var un;
+console.log(un)
+
+var un1;
+var un2;
+var ans = un1+un2; 
+console.log(ans)
+
+//intiatlize right variable names
+var myName = "Harsh";
+console.log(myName)
+
+//let vs const
+var nameNew = "Cody";
+console.log("1st:",nameNew)
+var nameNew = "Harsh"
+console.log("2nd",nameNew)
+
+//decalring a constant variable
+const PI_VALUE = 3.14159265359
+console.log(PI_VALUE)
